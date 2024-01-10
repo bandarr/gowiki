fbc5419a5b57ecea8dad703af6249f302ed0590c
Dan Barr <dan.barr@outlook.com>
2024-01-09 22:21:05 -0500

    we can load a page too!

--- wiki.go
+++ wiki.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 )
 
@@ -13,4 +12,10 @@ type Page struct {
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) *Page {
+	filename := title + ".txt"
+	body, _ := os.ReadFile(filename)
+	return &Page{Title: title, Body: body}
 }
